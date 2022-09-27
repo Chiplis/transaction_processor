74baff46ad34d57106ac22d45cfacb3cd0712d65
Nicolás Siplis <25803723+nicolas-siplis@users.noreply.github.com>
2022-09-27 20:07:12 +0200

    Separate CSV parsing from main method.

--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,7 @@ mod account;
 mod ledger;
 mod transaction;
 
+use std::borrow::Borrow;
 use crate::account::{Account, AccountId};
 use crate::ledger::Ledger;
 use crate::transaction::Transaction;
@@ -21,14 +22,34 @@ fn main() -> Result<(), Box<dyn Error>> {
         ))?
     }
 
+    let path = &args[1];
+    let mut accounts = HashMap::new();
+    let accounts = match process_csv(path, &mut accounts) {
+        Ok(accounts) => Ok(accounts),
+        Err(cause) => Err(format!("Unexpected error while attempting to parse CSV: {}", cause))
+    }?;
+
+    println!("client,available,held,total,locked");
+    accounts.iter().for_each(|(account_id, account)| {
+        println!(
+            "{account_id},{},{},{},{}",
+            account.available(),
+            account.held(),
+            account.total(),
+            account.locked()
+        );
+    });
+    Ok(())
+}
+
+fn process_csv<'a>(path: &'a String, accounts: &'a mut HashMap<AccountId, Account>) -> Result<&'a HashMap<AccountId, Account>, Box<dyn Error>> {
     let mut csv = ReaderBuilder::new()
         .has_headers(true)
-        .trim(Trim::All)
-        .flexible(true)
-        .from_path(Path::new(&args[1]))?;
+        .trim(Trim::All) // Supports arbitrary blank spaces between columns
+        .flexible(true) // Allows parsing of differently sized rows
+        .from_path(Path::new(&path))?;
 
     let mut ledger = Ledger::new();
-    let accounts: &mut HashMap<AccountId, Account> = &mut HashMap::new();
     let mut rows: usize = 2; // Start counting from the 2nd line since we skip the headers
     for row in csv.deserialize::<Transaction>() {
         match row {
@@ -40,16 +61,5 @@ fn main() -> Result<(), Box<dyn Error>> {
         }
         rows += 1;
     }
-
-    println!("client,available,held,total,locked");
-    accounts.iter().for_each(|(client, account)| {
-        println!(
-            "{client},{},{},{},{}",
-            account.available(),
-            account.held(),
-            account.total(),
-            account.locked()
-        );
-    });
-    Ok(())
+    Ok(accounts)
 }
